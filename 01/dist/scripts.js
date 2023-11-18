7f11fbbf6f1a7fd089f6afc6badb43e95342220c
Murilo M Andrade <andrade.murilom@gmail.com>
2023-11-18 15:30:26 -0300

    Exercicio 01 resolvido

--- 01/dist/scripts.js
+++ 01/dist/scripts.js
@@ -1,3 +1,4 @@
+"use strict";
 const usuarios = [
     {
         nome: "João",
@@ -20,21 +21,18 @@ const usuarios = [
         pets: ["Naninha"],
     },
 ];
-
 const buscarDonoPet = (lista, nomePet) => {
     let usuarioEncontrado;
-
     for (const usuario of lista) {
         if (usuario.pets.includes(nomePet)) {
             usuarioEncontrado = usuario;
         }
     }
-
     if (usuarioEncontrado) {
         console.log(`O dono(a) do(a) ${nomePet} é o(a) ${usuarioEncontrado.nome}`);
-    } else {
+    }
+    else {
         console.log(`Que pena ${nomePet}, não encontramos seu dono(a)`);
     }
-}
-
-buscarDonoPet(usuarios, 'Guido');
+};
+buscarDonoPet(usuarios, 'Lulu');
---
+++ 01/scripts.ts
@@ -0,0 +1,43 @@
+const usuarios: {
+    nome: string;
+    pets: string[];
+}[] = [
+        {
+            nome: "João",
+            pets: ["Max"],
+        },
+        {
+            nome: "Ana",
+            pets: ["Pingo", "Lulu"],
+        },
+        {
+            nome: "Beatriz",
+            pets: ["Lessie"],
+        },
+        {
+            nome: "Carlos",
+            pets: ["Farofa", "Salsicha", "Batata"],
+        },
+        {
+            nome: "Antonio",
+            pets: ["Naninha"],
+        },
+    ];
+
+const buscarDonoPet = (lista: { nome: string, pets: string[] }[], nomePet: string) => {
+    let usuarioEncontrado;
+
+    for (const usuario of lista) {
+        if (usuario.pets.includes(nomePet)) {
+            usuarioEncontrado = usuario;
+        }
+    }
+
+    if (usuarioEncontrado) {
+        console.log(`O dono(a) do(a) ${nomePet} é o(a) ${usuarioEncontrado.nome}`);
+    } else {
+        console.log(`Que pena ${nomePet}, não encontramos seu dono(a)`);
+    }
+}
+
+buscarDonoPet(usuarios, 'Lulu');
